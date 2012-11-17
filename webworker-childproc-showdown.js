c3a7cab58090d3ee7432a08e29bcf647573c3a40
David Ellis <d.f.ellis@ieee.org>
2012-11-16 17:44:24 -0800

    Switching to our fork for a bit to see if this fixes Linux.

--- webworker-childproc-showdown.js
+++ webworker-childproc-showdown.js
@@ -1,9 +1,14 @@
 var childproc = require('child_process');
 var webworker = require('webworker-threads').Worker;
-var count = process.argv[process.argv.length-1] || 100;
+var count = process.argv[process.argv.length-1] || 10;
 
 var thread = new webworker('./webworker.js');
 var proc = childproc.fork('./childproc.js');
+
+var testArr = new Array(1000000);
+for(var i = 0; i < 1000000; i++) {
+    testArr[i] = i;
+}
 
 var threadRuns = 0, startTime, threadEndTime, procRuns = 0, procEndTime;
 thread.onmessage = function(event) {
@@ -28,7 +33,6 @@ function runLoop(type) {
     startTime = new Date();
     if(type == 'process') {
         for(var i = 0; i < count; i++) {
-            var testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
             /*for(var j = 0; j < count; j++) {
                 testArr.push(i);
                 testArr.push(j);
@@ -37,7 +41,6 @@ function runLoop(type) {
         }
     } else {
         for(var i = 0; i < count; i++) {
-            var testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
             /*for(var j = 0; j < count; j++) {
                 testArr.push(i);
                 testArr.push(j);
